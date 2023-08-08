9c709a0bd216fd13e4d889163d35134fc334ff8c
Armaan Punj <apunj04@gmail.com>
2023-08-07 22:33:31 -0400

    initial build files

---
+++ build/precache-manifest.89fdb97423d45a76bfdbd2d3d2a57601.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bac29e650c08f727e21",
+    "url": "/punjportfolio/static/css/main.04a8d69d.chunk.css"
+  },
+  {
+    "revision": "3bac29e650c08f727e21",
+    "url": "/punjportfolio/static/js/main.3bac29e6.chunk.js"
+  },
+  {
+    "revision": "575592be2d3e253bb90c",
+    "url": "/punjportfolio/static/js/1.575592be.chunk.js"
+  },
+  {
+    "revision": "f419c139e155a5553c28",
+    "url": "/punjportfolio/static/js/2.f419c139.chunk.js"
+  },
+  {
+    "revision": "5b79beb1f5bc104f86b8",
+    "url": "/punjportfolio/static/js/runtime~main.5b79beb1.js"
+  },
+  {
+    "revision": "d35bedfd8c20e3be309e5af5195b0c59",
+    "url": "/punjportfolio/static/media/atl.d35bedfd.jpg"
+  },
+  {
+    "revision": "0f5fccd332c63c976dbe270f6f9d2914",
+    "url": "/punjportfolio/static/media/ukelele.0f5fccd3.JPG"
+  },
+  {
+    "revision": "538b93d25079be84ede0624b4be9502b",
+    "url": "/punjportfolio/static/media/unc.538b93d2.jpg"
+  },
+  {
+    "revision": "5e35522b8cc884bc7f802079ba512b52",
+    "url": "/punjportfolio/static/media/aura.5e35522b.jpg"
+  },
+  {
+    "revision": "acc41bc26f32da97d2effc51043e9938",
+    "url": "/punjportfolio/static/media/zen.acc41bc2.jpeg"
+  },
+  {
+    "revision": "eab63b78cbb3d9c3bebaff25b23db03f",
+    "url": "/punjportfolio/static/media/ny.eab63b78.JPG"
+  },
+  {
+    "revision": "2726c4e0093a1f0c6779b34503e92e86",
+    "url": "/punjportfolio/static/media/APlogo3.2726c4e0.png"
+  },
+  {
+    "revision": "9b999db01d2079763b80e1aab6f2bfab",
+    "url": "/punjportfolio/static/media/armaan.9b999db0.jpg"
+  },
+  {
+    "revision": "cc9f3b7d6f2c05540ab0a55798c5e3a4",
+    "url": "/punjportfolio/index.html"
+  }
+];
